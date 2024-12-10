2c160e61e8a63bf47d79ac376e19ec6b2a3393ac
ngoanlesqtt-create <149411455+ngoanlesqtt-create@users.noreply.github.com>
2024-12-10 15:53:55 +0700

    BreakWord

--- WordBreak/WordBreak/WordBreak/Program.cs
+++ WordBreak/WordBreak/WordBreak/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        HashSet<string> wordSet = new HashSet<string>(wordDict);
+
+        // Tạo mảng DP
+        int n = s.Length;
+        bool[] dp = new bool[n + 1];
+
+        // Base case: Chuỗi rỗng là hợp lệ
+        dp[0] = true;
+
+        // Duyệt qua từng chỉ số trong chuỗi
+        for (int i = 1; i <= n; i++)
+        {
+            // Kiểm tra các substring kết thúc tại i-1
+            for (int j = 0; j < i; j++)
+            {
+                if (dp[j] && wordSet.Contains(s.Substring(j, i - j)))
+                {
+                    dp[i] = true; // Cập nhật trạng thái dp[i]
+                    break; // Dừng vòng lặp sớm nếu đã tìm thấy kết quả
+                }
+            }
+        }
+
+        // Kết quả là trạng thái tại chỉ số n
+        return dp[n];
+    }
+}
--- WordBreak/WordBreak/WordBreak/obj/Debug/net8.0/WordBreak.AssemblyInfo.cs
+++ WordBreak/WordBreak/WordBreak/obj/Debug/net8.0/WordBreak.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordBreak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f79f9a049061557ecaa201b4b233bc021039d39")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordBreak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordBreak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
